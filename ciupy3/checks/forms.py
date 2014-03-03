419ae1ee0272d2f64b2eda0076c097aca8f07f22
Jannis Leidel <jannis@leidel.info>
2014-03-03 22:17:46 +0100

    Shorter shorter shorter

--- ciupy3/checks/forms.py
+++ ciupy3/checks/forms.py
@@ -5,7 +5,7 @@ from .models import Check
 
 class CheckForm(forms.ModelForm):
     requirements = forms.CharField(required=True, widget=forms.TextInput(
-        attrs={'placeholder': 'Project(s) or requirements.txt URL(s)'}))
+        attrs={'placeholder': 'Project(s) or requirements URL(s)'}))
 
     def clean_requirements(self):
         requirements = self.cleaned_data['requirements']

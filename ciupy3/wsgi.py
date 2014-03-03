4d44a8da30caf31ff838fd220748b3181eb01d4b
Jannis Leidel <jannis@leidel.info>
2014-03-03 20:47:40 +0100

    Added envdir.

--- ciupy3/wsgi.py
+++ ciupy3/wsgi.py
@@ -1,15 +1,46 @@
 """
 WSGI config for ciupy3 project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
 """
-
 import os
+from pathlib import Path
+
+env = os.environ.get('ENVDIR', 'dev')
+
+parent_dir = Path(__file__).parent.parent
+
+# if env == 'prod':
+#     import newrelic.agent
+#     newrelic.agent.initialize(str(parent_dir / 'newrelic.ini'))
+
+import envdir
+
+envdir.read(str(parent_dir / 'envs' / env))
+
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "ciupy3.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ciupy3.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
--- manage.py
+++ manage.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+import envdir
+from pathlib import Path
+
+here = Path(__file__).parent
+path = here / 'envs' / os.environ.get('ENVDIR', 'dev')
+envdir.read(str(path))
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ciupy3.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")

a9c94faaa0802d10acffa14f5360ce1ad55d3a57
Jannis Leidel <jannis@leidel.info>
2014-03-11 14:36:08 +0100

    Simplified requirements parsing.

--- ciupy3/checks/forms.py
+++ ciupy3/checks/forms.py
@@ -19,18 +19,15 @@ class CheckForm(forms.ModelForm):
         requirements = self.cleaned_data['requirements']
         # if it's potentially the content of a requirements file
         # we write it to a temporary file and return that as a file:// URL
-        if '\n' in requirements or '\r' in requirements:
-            split_requirements = filter_requirements(requirements.splitlines())
-            line_number = len(split_requirements)
-            if line_number > 1:
-                with tempfile.NamedTemporaryFile(delete=False) as tmp:
-                    tmp.write(bytes(requirements, 'UTF-8'))
-                return ['file://' + tmp.name]
-            elif line_number == 1:
-                # if someone accidentally added empty lines to a space seprated
-                return split_requirements[0]
+        split_requirements = filter_requirements(requirements.splitlines())
+        line_number = len(split_requirements)
+        if line_number > 1:
+            with tempfile.NamedTemporaryFile(delete=False) as tmp:
+                tmp.write(bytes(requirements, 'UTF-8'))
+            return ['file://' + tmp.name]
 
         # assume the requirements is comma, semicolon or space separated text
+        requirements = split_requirements[0]
         if ',' in requirements:
             requirements = requirements.split(',')
         elif ';' in requirements:

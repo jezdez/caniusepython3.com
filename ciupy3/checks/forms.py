55c52b4a6bd607ff9302ad3f5abe4b4960de1141
Jannis Leidel <jannis@leidel.info>
2014-03-11 14:20:31 +0100

    Handle single line requirements with empty line at the end better.

--- ciupy3/checks/forms.py
+++ ciupy3/checks/forms.py
@@ -6,6 +6,11 @@ from .models import Check
 placeholder = "Project name(s) or URL(s) of pip requirements file"
 
 
+def filter_requirements(requirements):
+    requirements = [requirement.strip() for requirement in requirements]
+    return list(filter(None, requirements))
+
+
 class CheckForm(forms.ModelForm):
     requirements = forms.CharField(required=True, widget=forms.Textarea(
         attrs={'placeholder': placeholder}))
@@ -15,17 +20,24 @@ class CheckForm(forms.ModelForm):
         # if it's potentially the content of a requirements file
         # we write it to a temporary file and return that as a file:// URL
         if '\n' in requirements or '\r' in requirements:
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_requirements:
-                tmp_requirements.write(bytes(requirements, 'UTF-8'))
-            requirements = ['file://' + tmp_requirements.name]
-        elif ',' in requirements:
+            split_requirements = filter_requirements(requirements.splitlines())
+            line_number = len(split_requirements)
+            if line_number > 1:
+                with tempfile.NamedTemporaryFile(delete=False) as tmp:
+                    tmp.write(bytes(requirements, 'UTF-8'))
+                return ['file://' + tmp.name]
+            elif line_number == 1:
+                # if someone accidentally added empty lines to a space seprated
+                return split_requirements[0]
+
+        # assume the requirements is comma, semicolon or space separated text
+        if ',' in requirements:
             requirements = requirements.split(',')
         elif ';' in requirements:
             requirements = requirements.split(';')
         else:
             requirements = requirements.split()
-        requirements = [requirement.strip() for requirement in requirements]
-        return list(filter(None, requirements))
+        return filter_requirements(requirements)
 
     class Meta:
         model = Check

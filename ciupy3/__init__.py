e1c9d9376acd09981d0fd8650c5e71e96e167f41
Jannis Leidel <jannis@leidel.info>
2014-03-15 21:53:44 +0100

    Disable monkey patching for distlib again to figure out if that causes the errors.

--- ciupy3/__init__.py
+++ ciupy3/__init__.py
@@ -7,11 +7,11 @@ base_project_url = 'http://projects.caniusepython3.com/'
 def get_project_data(name):
     url = '%s%s/%s/project.json' % (base_project_url, name[0].upper(), name)
     return util._get_external_data(url)
-util.get_project_data = get_project_data
+# util.get_project_data = get_project_data
 
 
 def get_package_data(name, version):
     url = ('%s%s/%s/package-%s.json' % (base_project_url, name[0].upper(),
                                         name, version))
     return util._get_external_data(url)
-util.get_package_data = get_package_data
+# util.get_package_data = get_package_data

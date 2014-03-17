d0f435180097b3c1ff9266919d5a8cd222d5767e
Jannis Leidel <jannis@leidel.info>
2014-03-17 14:40:08 +0100

    Improved admin UI.

--- ciupy3/checks/admin.py
+++ ciupy3/checks/admin.py
@@ -4,7 +4,9 @@ from .models import Check
 
 
 class CheckAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'created_at', 'started_at', 'finished_at',
+                    'unblocked')
+    ordering = ['-created_at']
 
 
 admin.site.register(Check, CheckAdmin)

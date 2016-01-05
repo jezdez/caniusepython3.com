8263c51168da934b5e47184f10bc8935b21cddb7
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-01-05 10:53:36 +0100

    Fix deprecation warnings.

--- ciupy3/checks/urls.py
+++ ciupy3/checks/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from ciupy3.checks.views import (CheckDetailView, CheckCreateView,
                                  ProjectDetailView, autocomplete)
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', CheckCreateView.as_view(), name='check_form'),
     url(r'^autocomplete/$', autocomplete, name='project-autocomplete'),
     url(r'^project/(?P<name>[^/]+)\.(?P<format>(json|svg|png))$',
@@ -14,4 +14,4 @@ urlpatterns = patterns('',
         ProjectDetailView.as_view(), name='project-detail'),
     url(r'^check/(?P<pk>[^/]+)$',
         CheckDetailView.as_view(), name='check-detail'),
-)
+]
--- ciupy3/urls.py
+++ ciupy3/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^', include('ciupy3.checks.urls')),
-    url(r'^admin/', include('admin_honeypot.urls')),
+    url(r'^admin/', admin.site.urls),
     url(r'^manage/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework'))
-)
+]

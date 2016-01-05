06a679789da8dd4118f0cc49cfc66fa4e4a3aee2
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-01-05 14:42:11 +0100

    Added default redis client.

--- ciupy3/settings/common.py
+++ ciupy3/settings/common.py
@@ -130,6 +130,9 @@ CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379/0',
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
         # 'OPTIONS': {
         #     'PARSER_CLASS': 'redis.connection.HiredisParser',
         # }

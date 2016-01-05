d87d36119143ad83a714bae016c5c341aec1235d
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-01-05 10:55:42 +0100

    Updated production settings.

--- ciupy3/settings/prod.py
+++ ciupy3/settings/prod.py
@@ -5,7 +5,6 @@ import os
 from .common import *  # noqa
 
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
@@ -14,29 +13,30 @@ SECURE_SSL_REDIRECT = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
-MIDDLEWARE_CLASSES += (
-    'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware',
-)
+# MIDDLEWARE_CLASSES += (
+#     'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware',
+# )
+#
+# INSTALLED_APPS += (
+#     'raven.contrib.django.raven_compat',
+# )
 
-INSTALLED_APPS += (
-    'raven.contrib.django.raven_compat',
-)
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['caniusepython3.com']
 
 PIPELINE_ENABLED = True
 
 STATIC_URL = '/assets/static/'
 MEDIA_URL = '/assets/media/'
 
-SENTRY_URL = os.environ.get('SENTRY_URL')
-RAVEN_CONFIG = {
-    'dsn': SENTRY_URL,
-}
+# SENTRY_URL = os.environ.get('SENTRY_URL')
+# RAVEN_CONFIG = {
+#     'dsn': SENTRY_URL,
+# }
 
-TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
+TEMPLATE[0]['OPTIONS']['debug'] = False
+TEMPLATE[0]['OPTIONS']['loaders'] = (
+    'django.template.loaders.cached.Loader', (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
-    )),
+    )
 )

47ea229bcb3f85deb68e38fe6c11f52ebb638466
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2016-07-19 08:47:35 +0200

    Upgrad requirements and fix settings

--- ciupy3/settings/common.py
+++ ciupy3/settings/common.py
@@ -35,14 +35,17 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'debug': True,
+            "builtins": [
+                "easy_pjax.templatetags.pjax_tags"
+            ],
             'context_processors': (
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
-                'django.core.context_processors.tz',
-                'django.core.context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
             )
         },
@@ -77,6 +80,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'staticflatpages.middleware.StaticFlatpageFallbackMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'easy_pjax.middleware.UnpjaxMiddleware'
 )
 
 ROOT_URLCONF = 'ciupy3.urls'
